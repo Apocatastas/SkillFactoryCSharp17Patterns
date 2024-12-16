4b8e06a98c5cb35261bfaf8ee18e10f2ff2d7c0c
ɐllɐ <molchanova.a.a@gmail.com>
2024-12-16 20:55:16 +0300

    task 17.1.4

---
+++ SkillFactoryCSharp17Patterns/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SkillFactoryCSharp17Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public string GetDayOfWeek2(int day)
+        {
+            switch (day)
+            {
+                case 1:
+                    return "Monday";
+                case 2:
+                    return "Tuesday";
+                case 3:
+                    return "Wednesday";
+                case 4:
+                    return "Thursday";
+                case 5:
+                    return "Friday";
+                case 6:
+                    return "Saturday";
+                case 7:
+                    return "Sunday";
+                default:
+                    throw new ArgumentOutOfRangeException("Неизвестный индекс дня недели. Введите число в диапазоне 1 - 7");
+            }
+        }
+    }
+
+
+    
+
+   
+}
+
+
+
