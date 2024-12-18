53f772ec02b797c0dbd03da96cc5967fb8902e04
ɐllɐ <molchanova.a.a@gmail.com>
2024-12-18 20:59:22 +0300

    task 17.6.4 - before refactoring

---
+++ SkillFactoryCSharp17Patterns/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SkillFactoryCSharp17Patterns
+{
+    public class Account
+    {
+        // тип учетной записи
+        public string Type { get; set; }
+
+        // баланс учетной записи
+        public double Balance { get; set; }
+
+        // процентная ставка
+        public double Interest { get; set; }
+    }
+}
+
--- SkillFactoryCSharp17Patterns/Program.cs
+++ SkillFactoryCSharp17Patterns/Program.cs
@@ -6,42 +6,8 @@ namespace SkillFactoryCSharp17Patterns
     {
         static void Main(string[] args)
         {
-            Average(1);
-            Average(2);
-            Average(3);
             Console.ReadKey();
         }
-
-        /// <summary>
-        /// Prints average temperature
-        /// </summary>
-        /// <param name="period">1 for daily, 2 for weekly, 3 for monthly</param>
-        private static void Average(byte period)
-        {
-            string periodStr = String.Empty;
-            string celsium = String.Empty;
-            switch (period)
-            {
-                case 2:
-                    periodStr = "weekly";
-                    celsium = (new Random().Next(10, 35)).ToString();
-                    break;
-                case 3:
-                    periodStr = "monthly";
-                    celsium = (new Random().Next(10, 40)).ToString();
-                    break;
-                default:
-                    periodStr = "daily";
-                    celsium = (new Random().Next(10, 30)).ToString();
-                    break;
-
-
-            }
-            Console.WriteLine("Welcome to the {0} forecast page", periodStr);
-            Console.WriteLine("The average temperature expects to be around {0} C\n", celsium);
-
-        }
-
     }
 }
 
